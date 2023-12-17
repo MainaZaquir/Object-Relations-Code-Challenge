160d99a19f0bd4ea1117e1855b0ee17c5e992825
Zaquir <108782708+MainaZaquir@users.noreply.github.com>
2023-12-17 18:02:54 +0300

    Created the class customer

---
+++ lib/customer/customer.py
@@ -0,0 +1,28 @@
+class Customer:
+    customers = []
+
+    def __init__(self, given_name, family_name):
+        self.given_name = given_name
+        self.family_name = family_name
+        self.reviews = []
+        self.__class__.customers.append(self)
+
+    def full_name(self):
+        return f"{self.given_name} {self.family_name}"
+
+    @classmethod
+    def all(cls):
+        return cls.customers
+
+    @classmethod
+    def find_by_name(cls, name):
+        for customer in cls.customers:
+            if customer.full_name() == name:
+                return customer
+
+    @classmethod
+    def find_all_by_given_name(cls, name):
+        return [customer for customer in cls.customers if customer.given_name == name]
+
+    def num_reviews(self):
+        return len(self.reviews)
